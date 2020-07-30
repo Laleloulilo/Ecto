d62a2927b8315ce68fc99aedd575339bb5ba8810
Belgarath <laleloulilo@hotmail.com>
2020-07-30 12:28:40 +0200

    Correction bug mineurs index de démarrage + templating

--- Rendu/index.php
+++ Rendu/index.php
@@ -1,17 +1,21 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Bienvenue sur Ecto</title>
+</head>
 <body>
 <div>
-<?php
-$timestamp_debut = microtime(true);
-require_once('../Moteur/Scheduler.php');
-processusGlobalGenerationSite();
-// timestamp en millisecondes de la fin du script
-$timestamp_fin = microtime(true);
-// différence en millisecondes entre le début et la fin
-$difference_ms = $timestamp_fin - $timestamp_debut;
-// affichage du résultat
-echo '<p class="timer">Génération de la page : ' . round($difference_ms * 1000, 1) . ' millisecondes.'."</p>";
-?>
+    <?php
+    require_once('../Moteur/Scheduler.php');
+    $timestamp_debut = microtime(true);
+    processusGlobalGenerationSite();
+    // timestamp en millisecondes de la fin du script
+    $timestamp_fin = microtime(true);
+    // différence en millisecondes entre le début et la fin
+    $difference_ms = $timestamp_fin - $timestamp_debut;
+    // affichage du résultat
+    ?>
+    <p class="timer">Génération de la page : <?= round($difference_ms * 1000, 1) ?> millisecondes.</p>;
 </div>
 </body>
 </html>
