b6b5dcc61f36ed541cadd439707bcf64e5a7274b
Belgarath <laleloulilo@hotmail.com>
2020-08-04 01:38:37 +0200

    Optimisation template htaccess

--- Moteur/Template/templateHtaccess.php
+++ Moteur/Template/templateHtaccess.php
@@ -1,13 +1,7 @@
 <IfModule mod_headers.c>
     Header always set X-FRAME-OPTIONS "DENY"
-</IfModule>
-<IfModule mod_headers.c>
     Header always set X-XSS-Protection "1; mode=block"
-</IfModule>
-<IfModule mod_headers.c>
     Header set Content-Security-Policy "script-src 'self' https://www.google.com"
-</IfModule>
-<IfModule mod_headers.c>
     Header always set X-Content-Type-Options "nosniff"
 </IfModule>
 <IfModule mod_deflate.c>
@@ -22,6 +16,7 @@
     AddOutputFilterByType DEFLATE application/javascript
     AddOutputFilterByType DEFLATE application/x-javascript
 </IfModule>
+
 <?php
 if (file_exists(REPERTOIRE_DESTINATION_RENDU_PHP . '/' . "404.php")) { ?>
     ErrorDocument 404 <?=ADRESSE_EXACTE_SITE?>/404.php
@@ -30,9 +25,11 @@ if (file_exists(REPERTOIRE_DESTINATION_RENDU_PHP . '/' . "403.php")) { ?>
     ErrorDocument 403 <?=ADRESSE_EXACTE_SITE?>/403.php
     <?php
 }
-if (REDIRECTION_HTTPS) { ?>
+?>
+<IfModule mod_rewrite.c>
+<?php if (REDIRECTION_HTTPS) { ?>
     RewriteEngine On
     RewriteCond %{HTTPS} !on
     RewriteRule ^.*$ https://%{SERVER_NAME}%{REQUEST_URI} [R=301,L]
 <?php } ?>
-
+</IfModule>
