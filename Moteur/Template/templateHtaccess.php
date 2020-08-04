86cc758df4f577f1c956111c2deeee485a1237f7
Belgarath <laleloulilo@hotmail.com>
2020-08-04 10:48:03 +0200

    Optimisation htaccess

--- Moteur/Template/templateHtaccess.php
+++ Moteur/Template/templateHtaccess.php
@@ -1,9 +1,4 @@
-<IfModule mod_headers.c>
-    Header always set X-FRAME-OPTIONS "DENY"
-    Header always set X-XSS-Protection "1; mode=block"
-    Header set Content-Security-Policy "script-src 'self' https://www.google.com"
-    Header always set X-Content-Type-Options "nosniff"
-</IfModule>
+
 <IfModule mod_deflate.c>
     AddOutputFilterByType DEFLATE text/plain
     AddOutputFilterByType DEFLATE text/html
@@ -16,7 +11,20 @@
     AddOutputFilterByType DEFLATE application/javascript
     AddOutputFilterByType DEFLATE application/x-javascript
 </IfModule>
-
+<IfModule mod_headers.c>
+    Header always set X-FRAME-OPTIONS "DENY"
+    Header always set X-XSS-Protection "1; mode=block"
+    Header set Content-Security-Policy "script-src 'self' https://www.google.com"
+    Header always set X-Content-Type-Options "nosniff"
+</IfModule>
+<IfModule mod_rewrite.c>
+    <?php if (REDIRECTION_HTTPS) { ?>
+        RewriteEngine On
+        RewriteCond %{HTTPS} !on
+        RewriteRule ^.*$ https://%{SERVER_NAME}%{REQUEST_URI} [R=301,L]
+        RewriteRule ^([/0-9a-zA-Z-]+)\.html$ test.php?resource=$1 [L,QSA]
+    <?php } ?>
+</IfModule>
 <?php
 if (file_exists(REPERTOIRE_DESTINATION_RENDU_PHP . '/' . "404.php")) { ?>
     ErrorDocument 404 <?=ADRESSE_EXACTE_SITE?>/404.php
@@ -26,10 +34,4 @@ if (file_exists(REPERTOIRE_DESTINATION_RENDU_PHP . '/' . "403.php")) { ?>
     <?php
 }
 ?>
-<IfModule mod_rewrite.c>
-<?php if (REDIRECTION_HTTPS) { ?>
-    RewriteEngine On
-    RewriteCond %{HTTPS} !on
-    RewriteRule ^.*$ https://%{SERVER_NAME}%{REQUEST_URI} [R=301,L]
-<?php } ?>
-</IfModule>
+Options All -Indexes
