b281b5b343295fcb3270ba0b6565057877d41656
Belgarath <laleloulilo@hotmail.com>
2021-04-04 13:21:34 +0200

    chargement asynchrone css - accélération du rendu

--- Moteur/Template/templateCorpsPage.php
+++ Moteur/Template/templateCorpsPage.php
@@ -9,7 +9,7 @@
     <meta property="og:url" content="<?=ADRESSE_EXACTE_SITE.'/'.$url.'.php' ?>" />
     <meta property="og:image" content="<?=ADRESSE_EXACTE_SITE.'/'.REPERTOIRE_IMAGE.'/'.NOM_IMAGE_OPEN_GRAPH ?>" />
     <link rel="icon" type="image/png" href="./Images/favicon.png"/>
-    <link rel="stylesheet" type="text/css" href="./style/main.css"/>
+    <link rel="stylesheet" type="text/css" href="./style/main.css" onload="this.media='all'"/>
     <title><?= $titre ?> : <?= NOM_DU_SITE ?></title>
 </head>
 <body>
